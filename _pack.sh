77683ca0d0bf0dd3ad63f35910dda4b096941fdb
Gábor Poczkodi <gabest11@gmail.com>
2025-09-18 01:25:05 +0200

    Update _pack.sh

--- _pack.sh
+++ _pack.sh
@@ -131,7 +131,6 @@ for part in rootfs app; do
     sections[$part]=$(echo "$info" | awk -v n="$part" '$0 ~ "Section" && $0 ~ n {print $2}')
 done
 
-# Just cd into it; do not delete
 cd "$folder_name" || { echo "Directory $folder_name does not exist."; exit 1; }
 
 # Build UBI for each partition
